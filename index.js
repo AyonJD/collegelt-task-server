121865203670ab10250b9de6f50b610f5139de78
Ayon <ayonjodder177@gmail.com>
2022-06-08 00:49:10 +0600

    Api done

---
+++ index.js
@@ -0,0 +1,47 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const uri = "mongodb+srv://taskData:EnsvcHwV2eQfmsf7@cluster0.y4mhh.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+        const db = client.db("usersData");
+        const userCollection = db.collection("userCollection");
+
+        // API to Run Server 
+        app.get("/", async (req, res) => {
+            res.send("Server is Running");
+        });
+
+        //Api to get all users
+        app.get("/users", async (req, res) => {
+            const users = await userCollection.find({}).toArray();
+            res.send(users);
+        });
+
+        //Api to get user by id
+        app.get("/users/:id", async (req, res) => {
+            const user = await userCollection.findOne({ _id: ObjectId(req.params.id) });
+            res.send(user);
+        });
+    }
+    finally {
+        // client.close(); 
+    }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
