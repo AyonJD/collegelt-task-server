8aaeac0814cce5cdf06fdc36d5bb12fd4135492d
Ayon <ayonjodder177@gmail.com>
2022-06-10 15:43:03 +0600

    'Environment-var-added'

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const uri = "mongodb+srv://taskData:EnsvcHwV2eQfmsf7@cluster0.y4mhh.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y4mhh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const run = async () => {
